494cffd3c3d5f9d16ce05b86c5e120d8727bd20b
unknown <yura.zhloba@.allstars.local>
2011-11-04 11:58:04 +0300

    decode double

--- src/com/yzh44yzh/scalaAmf/Amf.scala
+++ src/com/yzh44yzh/scalaAmf/Amf.scala
@@ -19,14 +19,14 @@ object Amf
             case 0x2 => (AmfType.BOOL, false)
             case 0x3 => (AmfType.BOOL, true)
             case 0x4 => (AmfType.INT, AmfInt.readInt(buf))
-            case 0x5 => (AmfType.INT, 0) // TODO DOUBLE
-            case 0x6 => (AmfType.INT, 0) // TODO STRING
-            case 0x7 => (AmfType.INT, 0) // TODO XMLDOC
-            case 0x8 => (AmfType.INT, 0) // TODO DATE
-            case 0x9 => (AmfType.INT, 0) // TODO ARRAY
-            case 0xa => (AmfType.INT, 0) // TODO OBJECT
-            case 0xb => (AmfType.INT, 0) // TODO XML
-            case 0xc => (AmfType.INT, 0) // TODO BYTEARRAY
+            case 0x5 => (AmfType.DOUBLE, buf.getDouble)
+            case 0x6 => (AmfType.STRING, "") // TODO STRING
+            case 0x7 => (AmfType.STRING, "") // TODO XMLDOC
+            case 0x8 => (AmfType.DATE, 0) // TODO DATE
+            case 0x9 => (AmfType.ARRAY, 0) // TODO ARRAY
+            case 0xa => (AmfType.OBJECT, 0) // TODO OBJECT
+            case 0xb => (AmfType.STRING, 0) // TODO XML
+            case 0xc => (AmfType.BYTEARRAY, 0) // TODO BYTEARRAY
             case _ => throw new Exception("invalid amf type " + tp)
         }
     }
