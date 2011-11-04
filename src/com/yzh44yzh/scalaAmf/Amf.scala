0d2f9d0ff0922bf43898411d44e17bd04474da05
unknown <yura.zhloba@.allstars.local>
2011-11-04 15:29:37 +0300

    refactor, move read/write Date to AmfDate

--- src/com/yzh44yzh/scalaAmf/Amf.scala
+++ src/com/yzh44yzh/scalaAmf/Amf.scala
@@ -23,7 +23,7 @@ object Amf
             case 0x5 => (AmfType.DOUBLE, buf.getDouble)
             case 0x6 => (AmfType.STRING, "") // TODO STRING
             case 0x7 => (AmfType.STRING, "") // TODO XMLDOC
-            case 0x8 => (AmfType.DATE, readDate(buf))
+            case 0x8 => (AmfType.DATE, AmfDate.read(buf))
             case 0x9 => (AmfType.ARRAY, 0) // TODO ARRAY
             case 0xa => (AmfType.OBJECT, 0) // TODO OBJECT
             case 0xb => (AmfType.STRING, 0) // TODO XML
@@ -49,39 +49,12 @@ object Amf
                 buf.putDouble(value._2.asInstanceOf[Double])
             case AmfType.DATE =>
                 buf.put(0x8 toByte)
-                writeDate(buf,  value._2.asInstanceOf[Date])
+                AmfDate.write(buf,  value._2.asInstanceOf[Date])
             case _ => throw new Exception("invalid amf type " + value._1)
         }
 
         buf.flip
         buf.position(0)
         buf
-    }
-
-    def readDate(buf : IoBuffer) : Date =
-    {
-        val ref = AmfInt.read(buf)
-
-        /*if((ref & 1) == 0)
-        {
-            // return getReference(ref >> 1).asInstanceOf[Date]
-        }*/
-
-        val date = new Date(buf.getDouble.asInstanceOf[Long])
-        //storeReference(date)
-        date
-    }
-
-    def writeDate(buf : IoBuffer, date : Date) =
-    {
-        /*if(hasReference(date))
-        {
-            putInteger(getReferenceId(date) << 1)
-            return
-        }*/
-        //storeReference(date)
-
-        buf.put(0x1 toByte)
-        buf.putDouble(date.getTime)
     }
 }
---
+++ src/com/yzh44yzh/scalaAmf/AmfDate.scala
@@ -0,0 +1,38 @@
+/**
+ * @author Yura Zhloba <yzh44yzh@gmail.com>
+ */
+
+package com.yzh44yzh.scalaAmf
+
+import org.apache.mina.core.buffer.IoBuffer
+import java.util.Date
+
+private object AmfDate
+{
+    def read(buf : IoBuffer) : Date =
+    {
+        val ref = AmfInt.read(buf)
+
+        /*if((ref & 1) == 0)
+        {
+            // return getReference(ref >> 1).asInstanceOf[Date]
+        }*/
+
+        val date = new Date(buf.getDouble.asInstanceOf[Long])
+        //storeReference(date)
+        date
+    }
+
+    def write(buf : IoBuffer, date : Date) =
+    {
+        /*if(hasReference(date))
+        {
+            putInteger(getReferenceId(date) << 1)
+            return
+        }*/
+        //storeReference(date)
+
+        buf.put(0x1 toByte)
+        buf.putDouble(date.getTime)
+    }
+}
