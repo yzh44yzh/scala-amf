059ce2b6f99654df3f42572a47d2a9ebcc47cd7d
Yura Zhloba <yzh44yzh@gmail.com>
2011-12-27 22:41:35 +0300

    add sample-server module

---
+++ sample-server/src/com/yzh44yzh/scalaAmf/SampleServer.scala
@@ -0,0 +1,12 @@
+package com.yzh44yzh.scalaAmf
+
+/**
+ * @author Yura Zhloba <yzh44yzh@gmail.com>
+ */
+
+import org.apache.mina.core._
+
+object SampleServer extends App
+{
+	println("Hello, world!")
+}
